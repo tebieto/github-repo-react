f58c47b1ca43bc67404d1eb9a88357132d21fa67
Terry Ebieto <tebieto@gmail.com>
2020-11-15 18:47:32 +0100

    added git ignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,7 +11,7 @@ const http = require('http').createServer(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV === 'production') app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 http.listen(port, error => {
@@ -19,9 +20,9 @@ http.listen(port, error => {
 });
 
 app.get('*', (req, res) => {
-	res.send('');
+	res.send('Unauthorized');
 });
 
-app.post('/api/key', (req, res) => {
+app.post('/api/key/github', (req, res) => {
 	res.send({ key: process.env.GITHUB_API_KEY });
 });
